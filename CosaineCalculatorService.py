34487dd34817a94f8fa51bab937189bbd31a805c
prueba <a@a.com>
2019-03-14 14:51:26 +0100

    Primer Commmit user prueba

--- CosaineCalculatorService.py
+++ CosaineCalculatorService.py
@@ -7,7 +7,16 @@ allusers = []
 pandaarray  = []
 pandacountarray = []
 
+# ratingArray - [0,5] Example: ratingArray = 3,2,4,1
+def getNorma(ratingArray):
+    sum = 0
+    for rateuser in ratingArray:
+        sum = sum + rateuser**2
+    return sum
+
+
 def getRatingFile():
+    print('estudiando para mejorar')
     df,allusers,pandaarray,pandacountarray = fr.DataSetBuilder()
     return df,list(allusers),pandaarray,pandacountarray
 
@@ -20,3 +29,5 @@ def generalDistanceMatrix(allusers,n):
             ## TO BE IMPLEMENTED
             print("null")
     return "null"
+
+getNorma([1,2,3,4,5])
